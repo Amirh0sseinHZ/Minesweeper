47412b4883b399b8f14f2ae3fe12cce8790c13a2
k1lleR8 <amirziaee12@gmail.com>
2020-08-06 22:31:20 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+import { Minesweeper } from "./Minesweeper.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const games = document.getElementsByClassName("minesweeper");
+  [...games].forEach(function (game) {
+    new Minesweeper({
+      el: game,
+      width: game.getAttribute("data-width") || 10,
+      height: game.getAttribute("data-height") || 10,
+      difficulty: game.getAttribute("data-difficulty") || "easy",
+      debug: game.getAttribute("data-debug") == "true" ? true : false || false,
+    });
+  });
+});
